7a808bca6829c4d8591f158640b6ef92c8931301
anakorjakov <a.nakorjakov@gmail.com>
2022-01-19 12:43:41 +0300

    Added handling .env file

--- generate-app.js
+++ generate-app.js
@@ -3,6 +3,7 @@
 const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const os = require("os");
 
 if (process.argv.length < 3) {
     console.log('You have to provide a name to your app.');
@@ -14,7 +15,10 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = 'git@github.com:anakorjakov/create-my-boilerplate-demo.git';
+const git_repo = 'https://github.com/anakorjakov/create-my-boilerplate-demo.git';
+const packagePathsByName = {};
+const envFilePath = path.resolve(projectPath, ".env");
+let readEnvVars = []
 
 try {
   fs.mkdirSync(projectPath);
@@ -27,19 +31,70 @@ try {
   process.exit(1);
 }
 
+/**
+ * Finds the key in .env files and returns the corresponding value
+ *
+ * @param {string} key Key to find
+ * @returns {string|null} Value of the key
+ */
+const getEnvValue = (key) => {
+  // find the line that contains the key (exact match)
+  const matchedLine = readEnvVars().find((line) => line.split("=")[0] === key);
+  // split the line (delimiter is '=') and return the item at index 2
+  return matchedLine !== undefined ? matchedLine.split("=")[1] : null;
+};
+
+/**
+ * Updates value for existing key or creates a new key=value line
+ *
+ * This function is a modified version of https://stackoverflow.com/a/65001580/3153583
+ *
+ * @param {string} key Key to update/insert
+ * @param {string} value Value to update/insert
+ */
+const setEnvValue = (key, value) => {
+  const envVars = readEnvVars();
+  const targetLine = envVars.find((line) => line.split("=")[0] === key);
+  if (targetLine !== undefined) {
+    // update existing line
+    const targetLineIndex = envVars.indexOf(targetLine);
+    // replace the key/value with the new value
+    envVars.splice(targetLineIndex, 1, `${key}="${value}"`);
+  } else {
+    // create new key value
+    envVars.push(`${key}="${value}"`);
+  }
+  // write everything back to the file system
+  fs.writeFileSync(envFilePath, envVars.join(os.EOL));
+};
+
+
+const updatePackageJson = () => {
+  const packageJson = path.join(projectPath, 'package.json');
+  const json = JSON.parse(fs.readFileSync(packageJson, 'utf8'));
+  json.name = projectName;
+  fs.writeFileSync(packageJson, JSON.stringify(json, null, 2), 'utf8');
+}
+
 async function main() {
     try {
       console.log('Downloading files...');
       execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
 
       process.chdir(projectPath);
+      updatePackageJson();
 
       console.log('Installing dependencies...');
       execSync('npm install');
 
-      console.log('Removing useless files);
+      console.log('Removing useless files');
       execSync('npx rimraf ./.git');
       fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true});
+
+      // read .env file & convert to array
+      readEnvVars = fs.readFileSync(envFilePath, "utf-8").split(os.EOL);
+      console.log(getEnvValue('SUPER_SECRET'));
+      setEnvValue('KEY_1', 'value 1')
 
       console.log('The installation is done, this is ready to use !');
 
